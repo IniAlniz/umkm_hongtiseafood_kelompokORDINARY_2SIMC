0967cb7e7f8d9e6ea21958679d3951cd8b7ae351
IniAlniz <nuralniziqlima29@gmail.com>
2024-06-14 16:28:28 +0700

    Add files via upload

---
+++ resources/views/admin/products/product-images/index.blade.php
@@ -0,0 +1,107 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Product Images') }} : {{$product->name}}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">                
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <div class="flex flex-column sm:flex-row flex-wrap space-y-4 sm:space-y-0 items-center justify-between pb-4">                       
+                        <div class="pb-4 bg-white dark:bg-gray-900">
+                            <label for="table-search" class="sr-only">Search</label>
+                            <div class="relative mt-1">
+                                <div class="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+                                    <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                                    </svg>
+                                </div>
+                                <input type="text" id="table-search" class="block pt-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for items">
+                            </div>
+                        </div>
+                        <div> <a href="{{route('admin.products.product-images.create', $product)}}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Add Product Image</a>
+                        </div>
+                        </div>
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="p-4">
+                                        <div class="flex items-center">
+                                            <input id="checkbox-all-search" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                            <label for="checkbox-all-search" class="sr-only">checkbox</label>
+                                        </div>
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Image
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Index
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Price
+                                    </th>                     
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @if (count($productImages) == 0)
+                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                        <td class="w-4 p-4 text-center" colspan="4">
+                                            --No Data--
+                                        </td>
+                                    </tr>
+                                @else
+                                    @foreach ($productImages as $productImage)
+                                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                        <td class="w-4 p-4">
+                                            <div class="flex items-center">
+                                                <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                                                <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                                            </div>
+                                        </td>
+                                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            <img class="h-auto max-w-xs" src="{{$productImage->image_url}}" alt="image description">
+                                        </th>
+                                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {{$productImage->indexing}}
+                                        </th>        
+                                        <td class="px-6 py-4">
+                                            <a href="{{route('admin.products.product-images.edit', [$product, $productImage])}}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                            <a x-data=""
+                                            x-on:click.prevent="$dispatch('open-modal', 'confirm-product-image-deletion-{{$productImage->id}}')" class="font-medium text-red-600 dark:text-red-500 hover:underline ms-2">Delete</a>
+
+                                            <x-modal name="confirm-product-image-deletion-{{$productImage->id}}" :show="$errors->productImageCategoryDeletion->isNotEmpty()" focusable>
+                                                <form method="post" action="{{ route('admin.products.product-images.destroy', [$product, $productImage]) }}" class="p-6">
+                                                    @csrf
+                                                    @method('delete')
+
+                                                    <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                                                        {{ __('Are you sure you want to delete this product image?') }}
+                                                    </h2>
+                                                    <h3>{{$product->name}}</h3>
+                                                    <img class="h-auto max-w-xs" src="{{$productImage->image_url}}" alt="image description">
+                                                    <div class="mt-6">
+
+                                                    <div class="mt-6 flex justify-end">
+                                                        <x-secondary-button x-on:click="$dispatch('close')">
+                                                            {{ __('Cancel') }}
+                                                        </x-secondary-button>
+
+                                                        <x-danger-button class="ms-3">
+                                                            {{ __('Delete') }}
+                                                        </x-danger-button>
+                                                    </div>
+                                                </form>
+                                            </x-modal>
+                                        </td>
+                                    </tr>
+                                    @endforeach
+                                @endif                                                                
+                            </tbody>
+                        </table>
+                    </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
